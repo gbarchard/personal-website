4c1c5ccb240bf8de9ad9788537b942a8ffbf5c3d
Grant Barchard <grant.barchard@gmail.com>
2023-03-25 16:20:06 -0400

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import Header from './components/Header'
 import { pages } from './pages'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import FooterSection from './components/Footer'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="dark:bg-gray-900 grow">
+      <div className="dark:bg-gray-900 grow flex flex-col overflow-hidden">
         <Header />
-        <div className="mx-8 my-6 lg:my-2">
-          <Routes>
-            {pages.map(page =>
-              <Route element={page.component} key={page.path} path={page.path} />
-            )}
-          </Routes>
+        <div className="grow overflow-scroll flex flex-col">
+          <div className="grow flex flex-col px-8 py-6 lg:py-2">
+            <Routes>
+              {pages.map(page =>
+                <Route element={page.component} key={page.path} path={page.path} />
+              )}
+            </Routes>
+          </div>
+          <FooterSection />
         </div>
       </div>
     </BrowserRouter>
