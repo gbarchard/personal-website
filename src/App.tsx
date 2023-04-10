dcd6b58ad0f449f00185e22d699c03d586c91860
Grant Barchard <grant.barchard@gmail.com>
2023-04-09 21:24:06 -0400

    fix scrolling on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <HashRouter basename={process.env.PUBLIC_URL}>
       <ToastContextProvider>
-        <div className="dark:bg-gray-900 grow flex flex-col overflow-hidden">
+        <div style={{ height: window.innerHeight }} className="dark:bg-gray-900 grow flex flex-col overflow-hidden">
           <Header />
           <div className="grow overflow-scroll flex flex-col">
             <div className="grow flex flex-col px-4 sm:px-12 md:px-32 lg:px-48 py-2 lg:py-6">
