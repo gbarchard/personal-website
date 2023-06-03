42b2256a1cb3c89e6102bbc851da444cf6ebd6b5
Grant Barchard <grant.barchard@gmail.com>
2023-06-03 16:03:24 -0400

    height changes on resize

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,21 @@ export default function App() {
   return (
     <HashRouter basename={process.env.PUBLIC_URL}>
       <ToastContextProvider>
-        <div style={{ height: window.innerHeight }} className="dark:bg-gray-900 grow flex flex-col overflow-hidden">
-          <Header />
-          <div className="grow overflow-scroll flex flex-col">
-            <div className="grow flex flex-col px-4 sm:px-12 md:px-32 lg:px-48 py-2 lg:py-6">
-              <Routes>
-                {pages.map((page) => (
-                  <Route
-                    element={page.component}
-                    key={page.path}
-                    path={page.path}
-                  />
-                ))}
-              </Routes>
-            </div>
-            <Contact />
-            <FooterSection />
+        <Header />
+        <div className="grow overflow-scroll flex flex-col">
+          <div className="grow flex flex-col px-4 sm:px-12 md:px-32 lg:px-48 py-2 lg:py-6">
+            <Routes>
+              {pages.map((page) => (
+                <Route
+                  element={page.component}
+                  key={page.path}
+                  path={page.path}
+                />
+              ))}
+            </Routes>
           </div>
+          <Contact />
+          <FooterSection />
         </div>
       </ToastContextProvider>
     </HashRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: 'media',
